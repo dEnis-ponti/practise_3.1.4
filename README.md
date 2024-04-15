79fa5a51c7621620ce7f65e71c1270aece98c71e
Denis Panteleev <delavaifag@gmail.com>
2024-04-15 15:39:14 +0300

    first version of working dynamic brand Logos

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# practise_3.1.4 
+# practise_5.1.3
 <br>Пантелеев Денис
-<br>**Практическая задача 3.1.4** Проверяемое задание «Верстка блока. Построение сеток + БЭМ именование»
-<br>**Ветка 3.1.4** > https://github.com/dEnis-ponti/practise_3.1.4/tree/main
+<br>**Практическая задача 5.1.3** Проверяемое задание «Верстка блока. JavaScript в браузере»
+<br>**Ветка 5.1.3** > https://github.com/dEnis-ponti/practise_3.1.4/tree/block_1.5
+
+https://denis-ponti.github.io/practise_3.1.4/
+^^^ <b>Git Pages Link</b> ^^^
+
+Ветки Предыдущих задач:
 
 <br>**Практическая задача 4.1.3** Проверяемое задание «Верстка блока. Продвинутая стилизация»
 <br>**Ветка 4.1.3** > https://github.com/dEnis-ponti/practise_3.1.4/tree/block_1.4
 
-https://denis-ponti.github.io/practise_3.1.4/
-^^^ <b>Git Pages Link</b> ^^^
+<br>**Практическая задача 3.1.3** Проверяемое задание «Верстка блока. Построение сеток + БЭМ именование»
+<br>**Ветка 3.1.3** > https://github.com/dEnis-ponti/practise_3.1.4/tree/main
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let brandsList = document.querySelector('.brands__list');
     let brandsItems = brandsList.children;
     let brandItem = document.querySelector('.brands__item');
-    
-    for (let i = 1; i <= 7; i++ ) {
+
+    for (let i = 1; i <= 7; i++) {
         let brandItemDuplicate = brandItem.cloneNode(true);
-        let brandLogo = brandItemDuplicate.querySelector('.brands__logo');
-        let brandLogoSrc = brandLogo.src;
-        
+
+
         brandsList.appendChild(brandItemDuplicate);
-        console.log(brandLogoSrc);
+        console.log(brandsItems);
+        let brandLogo = brandsItems[i].querySelector('.brands__logo');
+        console.log(brandLogo);
+        let brandLogoSrc = brandLogo.src;
+        let validBrandLogoSrc = brandLogoSrc.replace('brand-1.png', 'brand-' + (i + 1) + '.png');
+        console.log(validBrandLogoSrc);
+        brandLogo.src = validBrandLogoSrc;
+        console.log(brandsItems[i].brandLogoSrc);
+        console.log(brandsItems[i]);
     }
 });
