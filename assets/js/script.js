2a23f656b6417d35f818d6bfa9bd2d8aa2c9ad7d
Denis Panteleev <delavaifag@gmail.com>
2024-04-17 21:05:49 +0300

    working ver of slider and some styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,37 @@ document.addEventListener('DOMContentLoaded', function () {
     let brandsList = document.querySelector('.brands__list');
     let brandsItems = brandsList.children;
     let brandItem = document.querySelector('.brands__item');
+    let showMoreBtn = document.querySelector('.show-more-btn');
+    let showMoreBtnText = showMoreBtn.textContent;
 
-    for (let i = 1; i <= 7; i++) {
-        let brandItemDuplicate = brandItem.cloneNode(true);
+    let setSliderItems = function (numOfElems) {
+        for (let i = brandsItems.length; i <= numOfElems; i++) {
+            let brandItemDuplicate = brandItem.cloneNode(true);
+            brandsList.appendChild(brandItemDuplicate);
+            let brandLogo = brandsItems[i].querySelector('.brands__logo');
+            let brandLogoSrc = brandLogo.src;
+            let validBrandLogoSrc = brandLogoSrc.replace('brand-1.png', 'brand-' + (i + 1) + '.png');
+            brandLogo.src = validBrandLogoSrc;
+        }
+    };
+    setSliderItems(7);
 
-
-        brandsList.appendChild(brandItemDuplicate);
-        console.log(brandsItems);
-        let brandLogo = brandsItems[i].querySelector('.brands__logo');
-        console.log(brandLogo);
-        let brandLogoSrc = brandLogo.src;
-        let validBrandLogoSrc = brandLogoSrc.replace('brand-1.png', 'brand-' + (i + 1) + '.png');
-        console.log(validBrandLogoSrc);
-        brandLogo.src = validBrandLogoSrc;
-        console.log(brandsItems[i].brandLogoSrc);
-        console.log(brandsItems[i]);
-    }
+    showMoreBtn.addEventListener('click', function () {
+        if (!showMoreBtn.classList.contains('show-more-btn--active')) {
+            showMoreBtn.textContent = 'Скрыть';
+            showMoreBtn.classList.add('show-more-btn--active');
+            numOfElems = brandsItems.length + 2;
+            setSliderItems(numOfElems);
+        } else {
+            showMoreBtn.textContent = showMoreBtnText;
+            showMoreBtn.classList.remove('show-more-btn--active');
+            let itemsToDisplay = brandsItems.length - 3;
+            console.log(itemsToDisplay);
+            for (let j = brandsItems.length; j = itemsToDisplay; j - 1) {
+                console.log(j);
+                brandsItems[j].remove();
+                console.log(brandsItems[j]);
+            }
+        }
+    });
 });
