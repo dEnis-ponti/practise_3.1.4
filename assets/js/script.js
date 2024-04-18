cd649b5984ed1e23ab5d014be9fa6e520512ee3e
Denis Panteleev <delavaifag@gmail.com>
2024-04-18 21:44:28 +0300

    almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let brandsSlider = document.querySelector('.brands__slider')
     let brandsList = document.querySelector('.brands__list');
     let brandsItems = brandsList.children;
-    let brandItem = document.querySelector('.brands__item');
+    let brandItemTemplate = document.querySelector('#brand-template').content;
+    let brandItem = brandItemTemplate.querySelector('.brands__item');
     let showMoreBtn = document.querySelector('.show-more-btn');
     let showMoreBtnText = showMoreBtn.textContent;
 
@@ -15,23 +17,70 @@ document.addEventListener('DOMContentLoaded', function () {
             brandLogo.src = validBrandLogoSrc;
         }
     };
-    setSliderItems(7);
+
+    let adjustSliderItems = function () {
+        let screenWidth = window.innerWidth;
+        if (screenWidth >= 1120) {
+            setSliderItems(8);
+        } else if (screenWidth >= 768) {
+            setSliderItems(6);
+        } else {
+            setSliderItems(9)
+        }
+    }
+    adjustSliderItems();
+    window.addEventListener('resize', adjustSliderItems);
+
+    let initializeSwiper = function () {
+
+        let screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+
+            brandsSlider.classList.add('swiper', 'mySwiper');
+            brandsList.classList.add('swiper-wrapper');
+            for (let i = 0; i < brandsItems.length; i++) {
+                brandsItems[i].classList.add('swiper-slide');
+            }
+            console.log('swiper is initialized');
+            const swiper = new Swiper('.mySwiper', {
+                // Optional parameters
+                direction: 'horizontal',
+                loop: true,
+
+                // If we need pagination
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+
+            });
+        } else if (screenWidth >= 768) {
+            brandsSlider.classList.remove('swiper', 'mySwiper');
+            brandsList.classList.remove('swiper-wrapper');
+            for (let i = 0; i < brandsItems.length; i++) {
+                brandsItems[i].classList.remove('swiper-slide');
+            }
+
+        }
+    }
+    window.addEventListener('resize', initializeSwiper);
 
     showMoreBtn.addEventListener('click', function () {
         if (!showMoreBtn.classList.contains('show-more-btn--active')) {
             showMoreBtn.textContent = 'Скрыть';
             showMoreBtn.classList.add('show-more-btn--active');
-            numOfElems = brandsItems.length + 2;
+            let numOfElems = brandsItems.length + 2;
             setSliderItems(numOfElems);
         } else {
             showMoreBtn.textContent = showMoreBtnText;
             showMoreBtn.classList.remove('show-more-btn--active');
             let itemsToDisplay = brandsItems.length - 3;
             console.log(itemsToDisplay);
-            for (let j = brandsItems.length; j = itemsToDisplay; j - 1) {
-                console.log(j);
-                brandsItems[j].remove();
-                console.log(brandsItems[j]);
+            for (let j = brandsItems.length; j = itemsToDisplay; j--) {
+                if (brandsItems[j] != undefined) {
+                    brandsItems[j].remove();
+                } else {
+                    break;
+                }
             }
         }
     });
